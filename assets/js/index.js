81fe73e81f69621b5906f201bd46e147c5a1e1ef
Jay-Ubisse <joaquimubisse@gmail.com>
2023-01-27 17:00:42 +0200

    updating the slideshow and news section

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,30 +16,19 @@ $(document).ready(function () {
 /**Codigo para o slide show */
 
 let slideIndex = 1;
-showSlides(slideIndex);
+let n;
 
-// controle de proximo/anterior
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// controle de botoes (dots)
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+var timeout = 4000;
+var showSlides = function () {
+    slideIndex += 1;
+    n = slideIndex;
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
+setInterval(showSlides, timeout);
