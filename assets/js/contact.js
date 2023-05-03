ff317cf3cab2f94cd447e3816dd182390eeb660b
DinisM02 <dinismatavele02@gmail.com>
2023-05-03 11:10:26 +0200

    Actualização de contactos, nucleo de estudante e Monografias

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,5 @@
 // Initialize and add the map
-function initMap() {
+/*function initMap() {
     // The location of Uluru
     const uluru = { lat: -25.344, lng: 131.031 };
     // The map, centered at Uluru
@@ -14,4 +14,23 @@ function initMap() {
     });
   }
   
-  window.initMap = initMap;
+  window.initMap = initMap;*/
+
+  
+function initMap() {
+  // Define a posição do mapa
+  var myLatLng = {lat: -25.978727, lng: 32.569480};
+
+  // Cria um objeto "map" e define a posição e o zoom inicial
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  // Adiciona um marcador na posição definida
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Campus do Zimpeto'
+  });
+}
